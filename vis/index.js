83d471027fd6cc3abea4fc20123199092d44dc57
Jiaqi <blacker0lily311@gmail.com>
2022-11-14 17:01:53 +1100

    new filters, partially working as vis might not accept 2 filters for nodes/edges

--- vis/index.js
+++ vis/index.js
@@ -1,14 +1,12 @@
 import json from "./db.json" assert { type: "json" };
 
-const nodeFilterSelector = document.getElementById("nodeFilterSelect");
-
-const edgeFilters = document.getElementsByName("edgesFilter");
+const nodeFilters = document.getElementsByName("nodeFilter");
 
 function startNetwork(data) {
     const container = document.getElementById("mynetwork");
     const options = {};
     new vis.Network(container, data, options);
-}
+};
 
 const problems = json["problems"];
 const reductions = json["reductions"];
@@ -17,119 +15,61 @@ var nodes = new vis.DataSet();
 var edges = new vis.DataSet();
 
 for (var i = 0; i < problems.length; i++) {
+    // get a copy of the problem info
     var copy = JSON.parse(JSON.stringify(problems[i]));
+    // delete irrelavant/empty fields
     delete copy["id"];
+    if (copy["also_known_as"] === []) { delete copy["also_known_as"]; }
+    if (copy["introduced_at"] === []) { delete copy["introduced_at"]; }
+    if (copy["parameters"] === []) { delete copy["parameters"]; }
+    if (copy["algorithms"] === []) { delete copy["algorithms"]; }
+
     var st = JSON.stringify(copy);
     nodes.add({id: problems[i]["id"], label: problems[i]["name"], category: problems[i]["category"], title: st}); 
-}
+};
+
 for (var i = 0; i < reductions.length; i++) {
     edges.add({from: reductions[i]["input"], to: reductions[i]["output"], arrows: { to: { enabled: true, type: "arrow" }}})
-}
-
-// create a network
-/* 
-var container = document.getElementById("mynetwork");
-var data = {
-    nodes: nodes,
-    edges: edges,
 };
-var options = {};
-var network = new vis.Network(container, data, options);
-*/
-
 
 /**
  * filter values are updated in the outer scope.
  * in order to apply filters to new values, DataView.refresh() should be called
  */
-let nodeFilterValue = "";
-const edgesFilterValues = {
-   input: true,
-   output: true,
-   friend: true,
-};
-/*
-filter function should return true or false
-based on whether item in DataView satisfies a given condition.
-*/
-const nodesFilter = (node) => {
-    if (nodeFilterValue === "") {
-        return true;
-    }
-    switch (nodeFilterValue) {
-        case "A":
-        return node.category === "A";
-        case "B":
-        return node.category === "B";
-        case "C":
-        return node.category === "C";
-        case "D":
-        return node.category === "D";
-        default:
-        return true;
-    }
+
+const nodesFilterValues = {
+   A: true,
+   B: true,
+   C: true,
+   D: true,
 };
 
-const nodesView = new vis.DataView(nodes, { filter: nodesFilter });
+const nodesFilterValues2 = {
+    ImplementationAvailable: true,
+    ImplementationAUnavailable: true,
+};
+
+const nodesFilter = (node) => {
+    return nodesFilterValues[node.category];
+};
+
+const nodesFilter2 = (node) => {
+    return nodesFilterValues2[node.implemented];
+};
+
+const nodesView = new vis.DataView(nodes, { filter: nodesFilter, nodesFilter2 });
 const edgesView = new vis.DataView(edges);
 
 
-const form1 = document.getElementById("form1");
-form1.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const p = document.getElementById("searchterm").value;
-    console.log(p);
-    nodes.update({id: 10, label: p});
-    network.redraw();
-});
-
-const tick1 = document.getElementById("tick1");
-tick1.addEventListener('change', (event) => {
-    if (event.currentTarget.checked) {
-        nodes.update({id: 11, label: "Yes"});
-        network.redraw();
-    } else {
-        nodes.update({id: 11, label: "No"});
-        network.redraw();
-    }
-})
-
-var select = document.getElementById("select");
-select.addEventListener('change', (e) => {
-    var text = select.options[select.selectedIndex].text;
-    for (var i = 0; i < problems.length; i++) { 
-        var h = false;
-        if (text === "Implementation Available") {       
-            if (problems[i]["implemented"] === false) {
-                h = true;
-            }      
-        } else if (text === "Implementation Unavailable") {       
-            if (problems[i]["implemented"] === true) {
-                h = true;
-            }     
-        }
-        data.nodes.update([{id: problems[i]["id"], hidden: h}]);
-    }
-    network.redraw();
-})
-
-nodeFilterSelector.addEventListener("change", (e) => {
-// set new value to filter variable
-nodeFilterValue = e.target.value;
-/*
-        refresh DataView,
-        so that its filter function is re-calculated with the new variable
-    */
-nodesView.refresh();
-});
-
-edgeFilters.forEach((filter) =>
-filter.addEventListener("change", (e) => {
+nodeFilters.forEach((filter) => filter.addEventListener("change", (e) => {
     const { value, checked } = e.target;
-    edgesFilterValues[value] = checked;
-    edgesView.refresh();
-})
-);
+    if (value in nodesFilterValues) {
+        nodesFilterValues[value] = checked;
+    } else if (value in nodesFilterValues2) {
+        nodesFilterValues2[value] = checked;
+    }
+    nodesView.refresh();
+}));
 
 startNetwork({ nodes: nodesView, edges: edgesView });
   
