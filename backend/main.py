b34422674bcb9fe27d89214bf9ca40a5a906b88f
Jiaqi <jiaqizhu21@gmail.com>
2023-02-17 10:56:46 +1100

    s

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,21 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+'''
+@app.post()
+@app.put()
+@app.delete()
+@app.options()
+@app.head()
+@app.patch()
+@app.trace()
+
+'''
