d4118b42c6a3aa5790627f7bac8ece482767179f
Jiaqi <jiaqizhu21@gmail.com>
2023-02-23 16:49:02 +1100

    new database

--- backend/models.py
+++ backend/models.py
@@ -1,27 +1,23 @@
-import sqlalchemy
 from sqlalchemy import Boolean, Column, ForeignKey, Numeric, Integer, String, DateTime, func
 from sqlalchemy.orm import relationship
+
 from database import Base
 
 class Users(Base):
     __tablename__ = "users"
-    username = Column(String, primary_key=True, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    logged_in = Column(Boolean, nullable=False, default=False)
-    tags_owned = relationship("Tags")
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
 
-class Tags(Base):
-    __tablename__ = "tags"
-    id = Column(Integer, primary_key=True, unique=True, nullable=False)
-    title = Column(String, nullable=False)
-    region = Column(String, nullable=False)
-    location = Column(String, nullable=False)
-    image = Column(Integer, default=-1)
-    n_likes = Column(Integer, nullable=False, default=0)
-    song_uri = Column(String)
-    caption = Column(String, nullable=False)
-    time_posted = Column(DateTime(timezone=True), server_default=func.now())
-    time_edited = Column(DateTime(timezone=True), onupdate=func.now())
-    username = Column(String, ForeignKey('users.username'))\
-    
+
+class Problems(Base):
+    __tablename__ = "problems"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    also_known_as = Column(String)
+    introduced_at = Column(String)
+    parameters = Column(String)
+    algorithms = Column(String)
+    category = Column(String)
+    implemented = Column(Boolean)
