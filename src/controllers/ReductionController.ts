a4612c4c196afafa9af13dc2424ada7f904a8f84
Jiaqi <jiaqizhu21@gmail.com>
2023-04-24 16:10:42 +1000

    cleaned up unused folders

--- src/controllers/ReductionController.ts
+++
@@ -1,9 +0,0 @@
-import ErrorHandler from "../models/ErrorHandler";
-
-class ReductionController {
-  defaultMethod() {
-    throw new ErrorHandler(501, 'Not implemented method');
-  }
-}
-  
-export = new ReductionController();
